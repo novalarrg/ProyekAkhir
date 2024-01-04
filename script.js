126cb8ad2089cf1cdda32f0664f095c047e3bdc9
novalar <90921502+novalarrg@users.noreply.github.com>
2024-01-04 21:50:17 +0700

    Update script.js - image not found 4040

--- script.js
+++ script.js
@@ -2,42 +2,42 @@
 const dataCard = [{
         title: "UI/UX Bootcamp",
         content: "Desain UI/UX, membekali dengan pengetahuan dan keterampilan.",
-        imageUrl: `/ProyekAkhir/Assets/Image/UIUX_Kursus.png`
+        imageUrl: `/Assets/Image/UIUX_Kursus.png`
     },
     {
         title: "Mobile App Bootcamp",
         content: "Desain UI/UX, membekali dengan pengetahuan dan keterampilan.",
-        imageUrl: `/ProyekAkhir/Assets/Image/MobileApp_Kursus.png`
+        imageUrl: `/Assets/Image/MobileApp_Kursus.png`
     },
     {
         title: "Data Science Bootc",
         content: "Desain UI/UX, membekali dengan pengetahuan dan keterampilan.",
-        imageUrl: `/ProyekAkhir/Assets/Image/DataScience_Kursus.png`
+        imageUrl: `/Assets/Image/DataScience_Kursus.png`
     },
     {
         title: "Full Stack Web",
         content: "Desain UI/UX, membekali dengan pengetahuan dan keterampilan.",
-        imageUrl: "/ProyekAkhir/Assets/Image/FullStack_Kursus.png"
+        imageUrl: "/Assets/Image/FullStack_Kursus.png"
     },
     {
         title: "Cybersecurity Bootc",
         content: "Desain UI/UX, membekali dengan pengetahuan dan keterampilan.",
-        imageUrl: `/ProyekAkhir/Assets/Image/Cybersecurity_Kursus.png`
+        imageUrl: `/Assets/Image/Cybersecurity_Kursus.png`
     },
     {
         title: "DevOps Bootcamp",
         content: "Desain UI/UX, membekali dengan pengetahuan dan keterampilan.",
-        imageUrl: `/ProyekAkhir/Assets/Image/DevOps_Kursus.png`
+        imageUrl: `/Assets/Image/DevOps_Kursus.png`
     },
     {
         title: "Blockchain Dev Bootc",
         content: "Desain UI/UX, membekali dengan pengetahuan dan keterampilan.",
-        imageUrl: `/ProyekAkhir/Assets/Image/Blockchain_Kursus.png`
+        imageUrl: `/Assets/Image/Blockchain_Kursus.png`
     },
     {
         title: "Cloud Computing",
         content: "Desain UI/UX, membekali dengan pengetahuan dan keterampilan.",
-        imageUrl: `/ProyekAkhir/Assets/Image/CloudComputing_Kursus.png`
+        imageUrl: `/Assets/Image/CloudComputing_Kursus.png`
     }
 ];
 
